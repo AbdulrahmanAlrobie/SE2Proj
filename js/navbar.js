3fcbdf3a3c98d01c97ec5d583d095ee8da68b7a1
mabosaimi <132561114+MAbOsaimi@users.noreply.github.com>
2024-01-05 12:49:13 +0300

    Sotre displayName in localStorage in order to display it faster on the navbar, redesign

--- js/navbar.js
+++ js/navbar.js
@@ -1,40 +1,68 @@
-import { auth, db, } from "../js/auth.js";
-import { ref, onValue, } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-database.js";
+import { auth, db } from "../js/auth.js";
+import {
+  ref,
+  onValue,
+} from "https://www.gstatic.com/firebasejs/10.7.1/firebase-database.js";
 
+const userInfo = document.getElementById("user-info");
+// Check if the user's name is stored in localStorage
+const storedName = localStorage.getItem("userDisplayName");
+
+if (storedName) {
+  // If the user's name is stored, use it
+  userInfo.innerHTML = `
+         <a href="account.html" id="account">${storedName}</a>
+         <button id="sign-out-btn" class="link-navbar">Sign out</button>
+     `;
+} else {
+  // If the user's name is not stored, show the default state
+  userInfo.innerHTML = `
+         <a href="auth.html?form=login" id="log-in">Log in</a>
+         <button id="sign-up-btn" class="link-navbar" onclick="window.location.href ='auth.html?form=signup'">Sign up</button>
+     `;
+}
+
+// Add event listener for the "Sign out" button using event delegation
+userInfo.addEventListener("click", function (event) {
+  if (event.target.id === "sign-out-btn") {
+    auth
+      .signOut()
+      .then(() => {
+        // Clear the user's name from localStorage when they sign out
+        localStorage.removeItem("userDisplayName");
+        location.reload();
+      })
+      .catch((error) => {
+        alert(error.message);
+      });
+  }
+});
 
 auth.onAuthStateChanged(function (user) {
-  const userInfo = document.getElementById("user-info");
-
   if (user) {
     // User is signed in.
-    const userRef = ref(db, 'users/' + user.uid);
+    const userRef = ref(db, "users/" + user.uid);
 
     // Listen for changes in the user's data
     onValue(userRef, (snapshot) => {
       const data = snapshot.val();
       const userDisplayName = data.displayName;
 
+      // Store the user's name in localStorage
+      localStorage.setItem("userDisplayName", userDisplayName);
+
       // Update the UI
       userInfo.innerHTML = `
         <a href="account.html" id="account">${userDisplayName}</a>
         <button id="sign-out-btn" class="link-navbar">Sign out</button>
       `;
-      document
-        .getElementById("sign-out-btn")
-        .addEventListener("click", function () {
-          auth
-            .signOut()
-            .then(() => { })
-            .catch((error) => {
-              alert(error.message);
-            });
-        });
     });
   } else {
     // User is not signed in.
+
     userInfo.innerHTML = `
-      <a href="auth.html?form=login" id="log-in">Log in</a>
-      <button id="sign-up-btn" class="link-navbar" onclick="window.location.href ='auth.html?form=signup'">Sign up</button>
-    `;
+        <a href="auth.html?form=login" id="log-in">Log in</a>
+        <button id="sign-up-btn" class="link-navbar" onclick="window.location.href ='auth.html?form=signup'">Sign up</button>
+      `;
   }
 });
